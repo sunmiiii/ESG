8280e403a0a1e1deb02fb4b55fc3c549e34a4f5e
sunmiiii <me1but1you1@gmail.com>
2022-11-22 15:07:13 +0900

    하는중ㅠㅠ

--- Quest04/skeleton/desktop.js
+++ Quest04/skeleton/desktop.js
@@ -1,15 +1,109 @@
+/** 엄격모드 */
+'use strict';
+
+/**
+ * 요구사항은 다음과 같습니다.
+ 아이콘은 폴더와 일반 아이콘, 두 가지의 종류가 있습니다.
+ 아이콘들을 드래그를 통해 움직일 수 있어야 합니다.
+ 폴더 아이콘은 더블클릭하면 해당 폴더가 창으로 열리며, 열린 폴더의 창 역시 드래그를 통해 움직일 수 있어야 합니다.
+ 바탕화면의 생성자를 통해 처음에 생겨날 아이콘과 폴더의 개수를 받을 수 있습니다.
+ 여러 개의 바탕화면을 각각 다른 DOM 엘리먼트에서 동시에 운영할 수 있습니다.
+ Drag & Drop API를 사용하지 말고, 실제 마우스 이벤트(mouseover, mousedown, mouseout 등)를 사용하여 구현해 보세요!
+ */
+
+let body = document.querySelector('body');
+let desktop = document.querySelector('.desktop');
+let iconFile = ['./icon/folderImg.png', './icon/iconImg.png'];
+let imgs = [];
+
 class Desktop {
-	/* TODO: Desktop 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+
 };
 
+
+2
 class Icon {
-	/* TODO: Icon 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	constructor(number, srcNum) {
+		this.number = number;
+		this.srcNum = srcNum;
+		this.selectNum();
+	};
+
+	selectNum() {
+		for (var i = 0; i < this.number; i++) {
+			imgs[i] = new Image(); // 이미지객체생성
+			imgs[i].src = iconFile[this.srcNum]; // 페이지 로딩 될때 이미지 미리 로딩
+			desktop.appendChild(imgs[i]);
+		}
+		console.log(imgs);
+	};
 };
 
 class Folder {
-	/* TODO: Folder 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
+	/** 폴더 아이콘을 클릭했을 때 모달창 생성 */
+
+
 };
 
 class Window {
 	/* TODO: Window 클래스는 어떤 멤버함수와 멤버변수를 가져야 할까요? */
 };
+
+
+/** dom 요소 가져오기*/
+
+// var folderIconImg = document.createElement('img');
+// folderIconImg.setAttribute('class','folderIconImg');
+// folderIconImg.setAttribute('src','./icon/folderImg.png');
+// desktop.appendChild(folderIconImg);
+
+// var iconImg = document.createElement('img');
+// iconImg.setAttribute('class','iconImg');
+// iconImg.setAttribute('src','./icon/iconImg.png');
+// desktop.appendChild(iconImg);
+
+
+/** ******************폴더이미지 아이콘이미지 생성*********************** */
+
+// var folderNum = Number(prompt('몇개의 폴더를 받아올래용?', 1));
+// var iconNum = Number(prompt('몇개의 아이콘을 받아올래용?', 1));
+
+// /** 반복할 횟수를 매개변수 number에, 이미지 경로 배열에서 지정번호 가져옴 srcNum*/
+// function selectNum(number,srcNum){
+// 	for(var i = 0; i < number; i++){
+// 		imgs[i] = new Image(); // 이미지객체생성
+// 		imgs[i].src = iconFile[srcNum]; // 페이지 로딩 될때 이미지 미리 로딩
+// 		desktop.appendChild(imgs[i]);
+// 	}
+// };
+
+// /** 함수 사용해서 반복회수, 지정경로 정해줌 */
+// selectNum(folderNum, 0);
+// selectNum(iconNum, 1);
+
+/** ******************폴더이미지 아이콘이미지 생성*********************** */
+
+/** 드래그 해보자! */
+let dragging = false;
+
+
+let pTagModal = document.createElement('p');
+pTag.textContent = "행복한 자바스크립트^^";
+
+let modalBtn = document.createElement('button');
+modalBtn.value = '창 닫기';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
